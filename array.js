73660ff0025edfd12962f3638ad7f6592cde2953
RICARDO YAN SANTOS PAIVA <ricardopaiva6177@digitalcollege.internal>
2025-06-10 20:20:00 -0300

    array

---
+++ array.js
@@ -0,0 +1,22 @@
+//1.Criar
+let nomes = ['Ana', 'Bia', 'Carlos', 'Daniel', 'Eduardo']
+let carros = []
+
+
+let idades = [12,'24','25']
+//console.log(idades)
+//push: adiciona elementos no final
+//pop:remove o ultimo elemento
+//shift:remove o primeiro elemento
+
+// nomes.pop()
+// nomes.shift()
+// console.log(`${nomes[1]} ${nomes[2]}`)
+
+//5. buscar elementos (filtrar)
+//.6. percorrer os itens
+
+let frutas = ['Abacaxi', 'Goiaba', 'Manga', 'Maracujá', 'Kiwi']
+frutas.forEach(function (fruta){
+    console.log(fruta);
+})
